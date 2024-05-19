4a4bf8f888c34fae8f61a3eb10a59c3f00ac17df
juned <79745275+junaid11P@users.noreply.github.com>
2024-05-19 22:26:39 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int a,b,sum;
+printf("Enter the number 1:");
+scanf("%d",&a);
+printf("Enter the number 2:");
+scanf("%d",&b);
+sum=a+b;
+printf("The sum of two number is:%d",sum);
+}
