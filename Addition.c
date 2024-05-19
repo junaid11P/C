acd270fa6945a7898bb73493b3b34fa35ca8bfac
juned <79745275+junaid11P@users.noreply.github.com>
2024-05-19 22:36:38 +0530

    Update Addition.c

--- Addition.c
+++ Addition.c
@@ -2,11 +2,12 @@
 #include<math.h>
 void main()
 {
-  int a,b,sum;
+  int a;
+  float b,sum;
 printf("Enter the number 1:");
 scanf("%d",&a);
 printf("Enter the number 2:");
-scanf("%d",&b);
+scanf("%f",&b);
 sum=a+b;
-printf("The sum of two number is:%d",sum);
+printf("The sum of %d and %f is:%f",a,b,sum);
 }
