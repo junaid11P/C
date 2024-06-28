ad1be3c4fcba03b0f8d02bec5059e39617b2d6c6
juned <79745275+junaid11P@users.noreply.github.com>
2024-06-28 17:51:59 +0530

    Update Append a line to existing(example.txt).c

--- Append a line to existing(example.txt).c
+++ Append a line to existing(example.txt).c
@@ -1,2 +1,18 @@
 #include<stdio.h>
-int 
+int main()
+{
+  FILE *file;
+char filename[]="Example.txt";
+char line="This is a new line to append for existing example.txt file\n";
+
+file=fopen(filename,"a");
+if(file==NULL)
+{
+printf("Unable to open file.\n");
+return 1;
+}
+fputs(line,file);
+fclose(file);
+printf("Line appended to the file.\n");
+return 0;
+}
